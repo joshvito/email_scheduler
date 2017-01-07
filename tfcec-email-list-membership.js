310ddabdb1f7e2f058646e65099e2a2833dea89d
joshvito <josh.vito@gmail.com>
2017-01-06 22:01:06 -0500

    Updates email send for Jan 2017 distribution

--- tfcec-email-list-membership.js
+++ tfcec-email-list-membership.js
@@ -131,6 +131,66 @@ var emails_20161105 = ""+
 "zenzerhead@aol.com,"+
 "" ;
 
+var emails_20161219 = ""+
+"altifrons@aol.com,"+
+"angelman4@verizon.net,"+
+"bmtdj@yahoo.com,"+
+"bonniegiff@aol.com,"+
+"brokenboxj@gmail.com,"+
+"caseykesterson@live.com,"+
+"ceganyboy@webtv.net,"+
+"cjgray49@gmail.com,"+
+"cphardy85@gmail.com,"+
+"cuttinglight@gmail.com,"+
+"dawnmd@gmail.com,"+
+"diggindirt45@gmail.com,"+
+"dtr1991@hotmail.com,"+
+"extra-spicy@hotmail.com,"+
+"fishguy_99@yahoo.com,"+
+"fishman544@gmail.com,"+
+"frankmoreauonthetube@gmail.com,"+
+"gardneri@aol.com,"+
+"gregnski@cs.com,"+
+"haaksr7@yahoo.com,"+
+"jackmullen2001@yahoo.com,"+
+"jeksoldfalls@gmail.com,"+
+"jimfitz105@gmail.com,"+
+"jkrywalski@roadrunner.com,"+
+"josephmalikowski77@gmail.com,"+
+"josh.vito@gmail.com,"+
+"kilobob53@aol.com,"+
+"kingfish51@verizon.net,"+
+"mcjit99@aol.com,"+
+"miguelongo9@gmail.com,"+
+"mike@mikeswetpets.com,"+
+"pappaluigi@verizon.net,"+
+"petespoint@juno.com,"+
+"phantom621@hotmail.com,"+
+"phoffman3@rochester.rr.com,"+
+"Princess.donna@live.com,"+
+"qty10@aol.com,"+
+"rapidron36@verizon.net,"+
+"rayenzinna@yahoo.com,"+
+"renae1964@gmail.com,"+
+"risquediver@yahoo.com,"+
+"rjgbuffalo1@yahoo.com,"+
+"russ531@hotmail.com,"+
+"shflierl@gmail.com,"+
+"steve826@roadrunner.com,"+
+"t_russ2@yahoo.com,"+
+"tbam101@gmail.com,"+
+"tfcectom@gmail.com,"+
+"tfcprez2@aol.com,"+
+"tgroset@yahoo.com,"+
+"theduuz@aol.com,"+
+"tkoala529@aol.com,"+
+"triz316@verizon.net,"+
+"wildbill41361@aol.com,"+
+"wilkolaski@roadrunner.com,"+
+"wrobinson113@triad.rr.com,"+
+"zenzerhead@aol.com,"+
+"";
+
 var tfcecFriends = ""+
 "CNYASPres@gmail.com,"+ 
 "borstein@yahoo.com,"+ 
@@ -155,12 +215,12 @@ var tfcecFriends = ""+
 //    buz.getBcc();
 var TFCECEMAILS = function () {};
 
-TFCECEMAILS.prototype.getBcc = function () {
-  return emails;
-};
+// TFCECEMAILS.prototype.getBcc = function () {
+//   return emails;
+// };
 
-TFCECEMAILS.prototype.getBccPre201611 = function () {
-  return emails_20161105;
+TFCECEMAILS.prototype.getBcc = function () {
+  return emails_20161219;
 };
 
 TFCECEMAILS.prototype.getFriends = function () {
